ee14d84226c29944bd0d897bba40e010254aa1c1
Tejas Subramani <44034397+subramani-tejas@users.noreply.github.com>
2021-09-25 20:41:16 -0500

    dynamic array implementation

---
+++ com/codewithtejas/Arrays/Array.java
@@ -0,0 +1,70 @@
+package com.codewithtejas.Arrays;
+
+import java.util.Arrays;
+
+// dynamic array implementation
+public class Array {
+    private int[] array;
+    private int index;
+
+    public Array(int length) {
+        array = new int[length];
+    }
+
+    // insert number O(n)
+    public void insert(int num) {
+        // array full
+        if (index == array.length) {
+            int[] newArray = new int[index * 2];
+
+            for (int i = 0; i < index; i++) {
+                newArray[i] = array[i];
+            }
+            array = newArray;
+        }
+        array[index++] = num;
+    }
+
+    // remove at index
+    public void removeAt(int i) {
+        if (i < 0 || i >= index) {
+            System.out.println("Invalid index!");
+            throw new IndexOutOfBoundsException();
+        }
+
+        for (int j = i; j <= index; j++) {
+            array[j] = array[j + 1];
+        }
+        index--;
+    }
+
+    // index of number
+    public int indexOf(int num) {
+        for (int i = 0; i < index; i++) {
+            if (array[i] == num)
+                return i;
+        }
+        return -1;
+    }
+
+    // print array
+    public void print() {
+        int[] printArray = new int[index];
+        for (int i = 0; i < index; i++) {
+            printArray[i] = array[i];
+        }
+        System.out.println(Arrays.toString(printArray));
+    }
+
+    // main
+    public static void main(String[] args) {
+        var numbers = new Array(3);
+        numbers.insert(2); // 0
+        numbers.insert(54);// 1
+        numbers.insert(12);// 2
+        numbers.insert(33);// 3
+        // numbers.removeAt(0);
+        System.out.println(numbers.indexOf(33));
+        numbers.print();
+    }
+}
